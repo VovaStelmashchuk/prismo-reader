ff2eaf7cd99915cafe5c3ff09631553a5e44cbbf
Vova Stelmashchuk <smallstells@gmail.com>
2023-03-08 13:05:42 +0200

    Try implement deploy

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,13 @@
 plugins {
     kotlin("multiplatform") version "1.8.0"
     kotlin("plugin.serialization") version "1.8.10"
+    id("org.jetbrains.dokka") version "1.8.10"
+    id("maven-publish")
+    id("signing")
 }
 
 group = "org.mixdrinks"
-version = "1.0-SNAPSHOT"
+version = "0.2"
 
 repositories {
     mavenCentral()
@@ -43,4 +46,71 @@ kotlin {
         val nativeMain by getting
         val nativeTest by getting
     }
+}
+
+val dokkaOutputDir = "$buildDir/dokka"
+
+tasks.dokkaHtml {
+    outputDirectory.set(file(dokkaOutputDir))
+}
+
+val deleteDokkaOutputDir by tasks.register<Delete>("deleteDokkaOutputDirectory") {
+    delete(dokkaOutputDir)
+}
+
+val javadocJar = tasks.register<Jar>("javadocJar") {
+    dependsOn(deleteDokkaOutputDir, tasks.dokkaHtml)
+    archiveClassifier.set("javadoc")
+    from(dokkaOutputDir)
+}
+
+publishing {
+    repositories {
+        maven {
+            setUrl { "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/" }
+            credentials {
+                username = System.getenv("SONATYPE_USERNAME")
+                password = System.getenv("SONATYPE_PASSWORD")
+            }
+        }
+    }
+
+    publications {
+        withType<MavenPublication> {
+            artifact(javadocJar)
+            pom {
+                name.set("mixdrinks-core")
+                description.set("The core library of the mixdrinks project.")
+                url.set("https://github.com/MixDrinks/core")
+                licenses {
+                    license {
+                        name.set("MIT license")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+                issueManagement {
+                    system.set("Github")
+                    url.set("https://github.com/MixDrinks/core/issues")
+                }
+                scm {
+                    connection.set("https://github.com/MixDrinks/core.git")
+                    url.set("https://github.com/MixDrinks/core")
+                }
+                developers {
+                    developer {
+                        name.set("Vova Stelmashchuk")
+                        email.set("vovochkastelmashchuk@gmail.com")
+                    }
+                }
+            }
+        }
+    }
+}
+
+signing {
+    useInMemoryPgpKeys(
+        System.getenv("GPG_PRIVATE_KEY"),
+        System.getenv("GPG_PRIVATE_PASSWORD")
+    )
+    sign(publishing.publications)
 }
